6541e2c0d0568edb459571a46cd6237504f0b63f
Michael OBrien <michael@obrienlabs.org>
2026-01-04 02:51:42 +0000

    #4 - start SA change

--- google-app-engine-java/main.tf
+++ google-app-engine-java/main.tf
@@ -3,14 +3,8 @@
 # App Engine Admin API appengine.googleapis.com
 # Compute API compute.googleapis.com
 
-
 # https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/google_project.html
 
-#resource "google_project" "my_project" {
-#  name       = "obrienlabs-test"
-#  project_id = "obrienlabs-test-001"
-#  org_id     = "1775303217800"
-#}
 
 # https://registry.terraform.io/modules/terraform-google-modules/project-factory/google/latest
 # https://github.com/terraform-google-modules/terraform-google-project-factory/tree/v18.2.0/examples/simple_project
@@ -39,12 +33,62 @@ module "project-factory" {
   deletion_policy = "DELETE"
 }
 
+# https://github.com/terraform-google-modules/terraform-google-project-factory/blob/v18.2.0/examples/app_engine/main.tf
+
 module "app-engine" {
   source  = "terraform-google-modules/project-factory/google//modules/app_engine"
   version = "~> 18.0"
   project_id  = module.project-factory.project_id
   location_id = var.region
 }
+
+
+#resource "time_sleep" "wait_for_apis" {
+#  depends_on      = [project-factory.activate_apis]
+#  create_duration = "60s"
+#}
+
+# local for now - later in cicd project
+resource "google_artifact_registry_repository" "images" {
+  project       = module.project-factory.project_id
+  location      = var.region
+  repository_id = var.artifact_repo_id
+  format        = "DOCKER"
+  description   = "Docker images for GAE services"
+  #depends_on = [time_sleep.wait_for_apis]
+}
+
+#locals {
+#  cloudbuild_sa = "${google_project.app.number}@cloudbuild.gserviceaccount.com"
+#}
+
+#resource "google_project_iam_member" "cloudbuild_ar_writer" {
+#  project = google_project.app.project_id
+#  role    = "roles/artifactregistry.writer"
+#  member  = "serviceAccount:${local.cloudbuild_sa}"
+#  depends_on = [time_sleep.wait_for_apis]
+#}
+
+# Ensure the service agent exists, and grant it AR read (so gae can pull the image).
+# gae-sandbox-3z8g-8f97@appspot.gserviceaccount.com
+#resource "google_project_service_identity" "gae_service_identity" {
+#  provider = google-beta
+#  project  = module.project-factory.project_id
+#  service  = "appengine.googleapis.com"
+#  #epends_on = [time_sleep.wait_for_apis]
+#}
+
+# 
+#resource "google_project_iam_member" "gae_artifact_reader" {
+#  project = module.project-factory.project_id
+#  role    = "roles/artifactregistry.reader"
+#  #member  = "serviceAccount:${google_project_service_identity.gae_service_identity.email}"
+#  #"serviceAccount:project-service-account@gae-sandbox-3z8g-8f97.iam.gserviceaccount.com"
+#  # from terraform show
+#  member = module.app-engine.default_service_account
+#  depends_on = [google_artifact_registry_repository.images]
+#}
+
 
 #  michael@cloudshell:~/wse_github/gcp-landing-zone/gcp-projects (lz-ado-xyz-boot-ot)$ gcloud services list --project obrienlabs-sandbox2-ot-ccb4 | grep NAME
 #NAME: analyticshub.googleapis.com
--- google-app-engine-java/variables.tf
+++ google-app-engine-java/variables.tf
@@ -19,3 +19,15 @@ variable "region" {
   description = "region"
   default     = "northamerica-northeast1"
 }
+
+variable "artifact_repo_id" {
+  description = "Artifact Registry repository id (DOCKER format)."
+  type        = string
+  default     = "gae-images"
+}
+
+variable "disable_services_on_destroy" {
+  description = "If true, APIs will be disabled when Terraform destroys the google_project_service resources."
+  type        = bool
+  default     = false
+}
